932ad40a42b3a22e7ed20616aecca82c8876971a
Pedro Lima <pedrolima@Pedros-MacBook-Pro.local>
2023-09-29 01:07:07 -0300

    Adding nav links and page transition with react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,31 @@ import React from 'react';
 import './App.css';
 import WeatherMap from './components/WeatherMap';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes }  from 'react-router-dom';
 
+const Home = () => <h2>Home Page</h2>;
 
-function App() {
-  return (
+const App = () => (
+  <Router>
     <div className="App">
-      {/* <h1>Wavy</h1> */}
       <Navbar />
-      <WeatherMap />
-     
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/weather-map" element={<WeatherMap />} />
+      </Routes>
     </div>
-  );
-}
+  </Router>
+);
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       {/* <h1>Wavy</h1> */}
+//       <Navbar />
+//       <WeatherMap />
+     
+//     </div>
+//   );
+// }
 
 export default App;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,29 @@
 // src/Navbar.js
 import React from 'react';
-import '../style/Navbar.css'
+import { Link, useLocation } from 'react-router-dom'; // Import Link and useLocation from react-router-dom
+import '../style/Navbar.css';
 
-function Navbar() {
+const Navbar = () => {
+  const location = useLocation(); // Get the current location
+
   return (
     <div className="navbar">
       <div className="logo">
         <h1>Wavy</h1>
         {/* <img src="logo.png" alt="Website Logo" /> */}
+      </div>
+      <div className="nav-links">
+        <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
+          Home
+        </Link>
+        <Link
+          to="/weather-map"
+          className={
+            location.pathname === '/weather-map' ? 'active' : ''
+          }
+        >
+         Wavy Map
+        </Link>
       </div>
       <div className="time-range-selector">
         {/* Add your time range selector here */}
