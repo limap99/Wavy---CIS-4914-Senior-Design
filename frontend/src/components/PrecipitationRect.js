fb1927c9e890ade187545b10bd7b051bc522f7d0
Pedro Lima <pedrolima@Pedros-MacBook-Pro.local>
2023-10-18 18:46:02 -0300

    Changed design of metric scale ruler

--- frontend/src/components/PrecipitationRect.js
+++ frontend/src/components/PrecipitationRect.js
@@ -3,30 +3,28 @@ import '../style/TemperatureRect.css';
 const PrecipitationRect = () => {
     return (
       <div className="colored-rectangle">
-        <div className="color prep7">
-          <span className="value-prep">0 mm</span>  
-        </div>
-        <div className="color prep6">
-          <span className="value-prep">1.5 mm</span>
-        </div>
-        <div className="color prep5">
-          <span className="value-prep">2 mm</span>
-        </div>
-        <div className="color prep4">
-          <span className="value-prep">3 mm</span>
-        </div>
-        <div className="color prep3">
-          <span className="value-prep">7 mm</span>
+        <div className="color prep1">
+          <span className="value-prep">0.2 in</span>
         </div>
         <div className="color prep2">
-          <span className="value-prep">10 mm</span>
+          <span className="value-prep">0.1 in</span>
         </div>
-        <div className="color prep1">
-          <span className="value-prep">20 mm</span>
+        <div className="color prep3">
+          <span className="value-prep">0.07 in</span>
         </div>
-        
+        <div className="color prep4">
+          <span className="value-prep">0.03 in</span>
+        </div>
+        <div className="color prep5">
+          <span className="value-prep">0.02 in</span>
+        </div>
+        <div className="color prep6">
+          <span className="value-prep">0.015 in</span>
+        </div>
+        <div className="color prep7">
+          <span className="value-prep">0 in</span>  
+        </div>       
       </div>
     );
   };
-  
   export default PrecipitationRect;
--- frontend/src/components/TemperatureRect.js
+++ frontend/src/components/TemperatureRect.js
@@ -3,29 +3,30 @@ import '../style/TemperatureRect.css';
 const TemperatureRect = () => {
     return (
       <div className="colored-rectangle">
-        <div className="color red">
-          <span className="value">-5&deg;F</span>  
+        <div className="color temp8">
+          <span className="value">100&deg;F</span>
         </div>
-        <div className="color green">
-          <span className="value">10&deg;F</span>
-        </div>
-        <div className="color blue">
-          <span className="value">25&deg;F</span>
-        </div>
-        <div className="color yellow">
-          <span className="value">40&deg;F</span>
-        </div>
-        <div className="color orange">
-          <span className="value">55&deg;F</span>
-        </div>
-        <div className="color purple">
-          <span className="value">70&deg;F</span>
-        </div>
-        <div className="color pink">
+        <div className="color temp7">
           <span className="value">85&deg;F</span>
         </div>
-        <div className="color brown">
-          <span className="value">100&deg;F</span>
+        <div className="color temp6">
+          <span className="value">70&deg;F</span>
+        </div>
+        <div className="color temp5">
+          <span className="value">55&deg;F</span>
+        </div>
+        <div className="color temp4">
+          <span className="value">40&deg;F</span>
+        </div>
+        <div className="color temp3">
+          <span className="value">25&deg;F</span>
+        </div>
+        <div className="color temp2">
+          <span className="value">10&deg;F</span>
+        </div>
+        
+        <div className="color temp1">
+          <span className="value">-5&deg;F</span>  
         </div>
       </div>
     );
